033fbd95466612605f266e99ab4a0a68ae5b324c
Alan Alonso <alanalonso@pop-os.localdomain>
2024-03-03 20:41:40 -0300

    first commit...

---
+++ __init__.py
@@ -0,0 +1,44 @@
+from flask import Flask, send_from_directory, url_for, request
+from flask_cors import CORS
+from flask_mail import Message, Mail
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_SECRET_KEY')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
+
+mail = Mail(app)
+
+@app.route("/articles")
+def articles():
+    return send_from_directory("static","articles.json")
+
+@app.route("/send_email",methods=['POST'])
+def send_email():
+    name = request.form['name']
+    subject = request.form['subject']
+    email = request.form['email']
+
+    msg = Message(subject,
+      sender=email,
+      recipients=[ os.getenv("MAIL_USERNAME") ])
+
+    msg.html = f"""<div>
+                    <h1>Nuevo mensaje desde portafolio</h1>
+                        <ul>
+                            <li>{name}</li>
+                            <li>{subject}</li>
+                            <li>{email}</li>
+                        </ul>
+                    </div>"""
+
+    mail.send(msg)
+    return "Email  enviado correctamente"
